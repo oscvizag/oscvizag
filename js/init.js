50ea1a740310ac6c6a236778b7614c70880cde76
nitishsai9 <kommaraju.nitish.9@gmail.com>
2019-07-21 13:49:01 +0530

    events,icons update

--- js/init.js
+++ js/init.js
@@ -33,7 +33,15 @@
         $('a').smoothScroll({
         speed:500,
         });
-     
+        $('.event .owl-carousel').owlCarousel({
+          autoplay:true,
+          margin:20,
+          responsive:{
+                0:{items:1,},
+              600:{items:2,},
+             1000:{items:3}
+          }
+         });
         
         html_body.on("click",function(){
           "use strict";
